411851618fc7c823b99a7ab590d356c809552e38
Appy Tong <appy@Appys-MacBook-Pro-2.local>
2025-12-22 08:36:49 +0100

    ruff reformat

--- src/zephyr/math_utils.py
+++ src/zephyr/math_utils.py
@@ -5,9 +5,11 @@ def add(a: int, b: int) -> int:
 def multiply(a: int, b: int) -> int:
     return a * b
 
+
 def multiply_only_positive(a: int, b: int) -> int:
     assert a >= 0 and b >= 0, "Only positive numbers allowed"
     return a * b
+
 
 class Calculator:
     def save(self, value: int) -> None:
